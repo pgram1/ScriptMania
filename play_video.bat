4addfaea946a3b363d9de9d01cf5bae0d11e9521
Petros Grammatikopoulos <pgrammatikopoulos@citycollege.sheffield.eu>
2019-10-02 23:22:43 +0300

    Add files via upload

--- play_video.bat
+++ play_video.bat
@@ -2,8 +2,10 @@
 :check
 set /p Url=What is the address of the video file/playlist you want me to play? Link: 
 echo Alright!
+youtube-dl -F "%Url%"
+set /p Qual=What quality? : 
 :replay
-mpv "%Url%"
+mpv --ytdl-format %Qual% "%Url%"
 set /p Cond=Press e to play something else, r to replay the song or any other key to terminate: 
 IF "%Cond%"=="e" (goto :check)
 IF "%Cond%"=="r" (goto :replay)
