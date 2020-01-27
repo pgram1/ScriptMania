bb3dcbfcbe111c7ead7cf8f78eaea7a9a891622e
Petros Grammatikopoulos <pgrammatikopoulos@citycollege.sheffield.eu>
2020-01-28 01:13:23 +0200

    Update play_video.bat

--- play_video.bat
+++ play_video.bat
@@ -1,12 +1,12 @@
 @echo off
 :check
-set /p Url=What is the address of the video file/playlist you want me to play? Link: 
+set /p Url=What is the address of the stream's file/playlist? Link/URL: 
 echo Alright!
 youtube-dl -F "%Url%"
-set /p Qual=What quality? : 
+set /p Qual=What quality?(video+audio or single stream): 
 :replay
 mpv --ytdl-format %Qual% "%Url%"
-set /p Cond=Press e to play something else, r to replay the song or any other key to terminate: 
+set /p Cond=Press e to play something else, r to replay the stream or any other key to terminate: 
 IF "%Cond%"=="e" (goto :check)
 IF "%Cond%"=="r" (goto :replay)
 PAUSE
