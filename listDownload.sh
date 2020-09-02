69e3ece0732d489cd8e32c3e4a99c78b51628a53
Petros Grammatikopoulos <pgrammatikopoulos@citycollege.sheffield.eu>
2020-09-02 03:30:29 +0300

    Update listDownload.sh

--- listDownload.sh
+++ listDownload.sh
@@ -1,3 +1,3 @@
-wget -i "$1"
+wget -U 'Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.8.1.6) Gecko/20070802 SeaMonkey/1.1.4' -e robots=off -i "$1"
 for file in *; do mv "$file" "$(basename "$file")$2"; done;
 mv "$1$2" "$1"
