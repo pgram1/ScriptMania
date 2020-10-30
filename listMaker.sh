55cb914252b13f6fa72c56a584b6fc8b1d9191f4
Petros Grammatikopoulos <pgram1.git@gmail.com>
2020-10-30 16:36:16 +0200

    Add files via upload

--- listMaker.sh
+++ listMaker.sh
@@ -1,1 +1,14 @@
-sed -n "/<div class=\"$2\">/,/<div class=\"$3\">/p" $1 > out
+#!/bin/bash
+
+# Search for "clearfix pbm".
+# Replace $2 and $3 with the correct classes from the html document (second and third or middle and right)
+
+sed -n "/<div class=\"$2\">/,/<div class=\"$3\">/p" $1 > out;
+
+tr -d "\n\r" < out > outTrimmed;
+
+grep -Po '(?<=\" href\=\")[^\"\#\&]+' outTrimmed | uniq > outProfileLinks;
+
+sed -e "s/?[^id].*//g" -i outProfileLinks;
+
+grep -Po '(?=https\:\/\/scontent)[^\"]+' outTrimmed > outProfileImages;
