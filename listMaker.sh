aaf72ea01efa2f2235494a04f131ddead4588210
Petros Grammatikopoulos <pgram1.git@gmail.com>
2020-10-30 20:55:42 +0200

    Add files via upload

--- listMaker.sh
+++ listMaker.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
 # Search for "clearfix pbm".
-# Replace $2 and $3 with the correct classes from the html document (second and third or middle and right)
+# Replace $2 with the correct classes from the html document
 
-sed -n "/<div class=\"$2\">/,/<div class=\"$3\">/p" $1 > out;
+sed -n "/<div aria-label\=\"Search Results\" role\=\"main\"/,/<div class=\"$2\">/p" $1 > out;
 
 tr -d "\n\r" < out > outTrimmed;
 
-grep -Po '(?<=\" href\=\")[^\"\#\&]+' outTrimmed | uniq > outProfileLinks;
+grep -Po '(?<=\" href\=\"https\:\/\/www.facebook.com\/)[^\"\#\&]+' outTrimmed | uniq > outProfileLinks;
 
 sed -e "s/?[^id].*//g" -i outProfileLinks;
 
-grep -Po '(?=https\:\/\/scontent)[^\"]+' outTrimmed > outProfileImages;
+sed -e 's/^/https\:\/\/www.facebook.com\//' outProfileLinks | uniq > outProfileLinks;
+
+grep -Po "(?<=\<image style\=\"height\: 60px\; width\: 60px\;\" x\=\"0\" y\=\"0\" height\=\"100\%\" preserveAspectRatio\=\"xMidYMid slice\" width\=\"100\%\" xlink:href\=\")[^\"]+" outTrimmed > outProfileImages;
